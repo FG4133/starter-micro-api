54671d1c1a403c89d3784e8aafcdf5fc1a5f9619
FG4133 <123200450+FG4133@users.noreply.github.com>
2023-02-18 21:03:06 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log(request.url);
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+  console.log(request.url); // This line will print the requested URL in the console
+  response.end();
+});
+
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
