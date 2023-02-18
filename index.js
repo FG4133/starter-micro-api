2d77677da5de0ac5311ab2d6332d0899cfa4bdcc
FG4133 <123200450+FG4133@users.noreply.github.com>
2023-02-18 20:41:28 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+export default {
+  async fetch(req) {
+    try {
+      const url = new URL(req.url);
+      const splitted = url.pathname.replace(/^\/*/, '').split('/');
+      const address = splitted[0];
+      url.pathname = splitted.slice(1).join('/');
+      url.hostname = address;
+      url.protocol = 'https';
+      return fetch(new Request(url, req));
+    } catch (e) {
+      return new Response(e);
+    }
+  }
+};
