e9e15f3fd14ab8d931d2350c6991665507d197d3
FG4133 <123200450+FG4133@users.noreply.github.com>
2023-02-18 21:00:06 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-const { URL, Request, Response } = require('node-fetch');
-const fetch = require('node-fetch');
-
-module.exports = {
-  async fetch(req) {
-    try {
-      const url = new URL(req.url);
-      const splitted = url.pathname.replace(/^\/*/, '').split('/');
-      const address = splitted[0];
-      url.pathname = splitted.slice(1).join('/');
-      url.hostname = address;
-      url.protocol = 'https:';
-      return await fetch(new Request(url, req));
-    } catch (e) {
-      return new Response(e);
-    }
-  }
-};
+console.log(request.url);
